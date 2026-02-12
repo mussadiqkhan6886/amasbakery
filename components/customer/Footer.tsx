f039c1eaeb7a8f373309b188227f5fa31ce1c048
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-02-12 15:32:35 +0500

    now translating

--- components/customer/Footer.tsx
+++ components/customer/Footer.tsx
@@ -1,9 +1,27 @@
+import { playFair } from '@/lib/fonts'
+import Link from 'next/link'
 import React from 'react'
+import { FiInstagram } from 'react-icons/fi'
 
 const Footer = () => {
   return (
-    <footer>
-      
+    <footer className='pt-7 flex flex-col'>
+      <div className="text-center flex items-center justify-center p-2">
+        <FiInstagram />
+      </div>
+      <div className='flex justify-between py-5 items-center'>
+        <div className='flex flex-col gap-2 text-sm'>
+          <Link href={"/"}>Shipping & Returns</Link>
+          <Link href={"/"}>Terms & Condition</Link>
+          <Link href={"/"}>Privacy Policy</Link>
+        </div>
+        <h6 className={`${playFair.className} text-7xl text-center `}>Amas bakery</h6>
+        <div className="text-sm">
+          <p>Based in Al Khobar</p>
+          <p>Amas Bakery Est. 2023</p>
+        </div>
+      </div>
+      <p className='text-center border-t border-main py-2'>&copy; All rights reserved {new Date().getFullYear()}</p>
     </footer>
   )
 }
