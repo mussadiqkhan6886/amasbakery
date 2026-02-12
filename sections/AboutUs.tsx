aa39211acf262d0e5a4b861d6a94a92b11a27a77
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-02-12 15:52:28 +0500

    done about

--- sections/AboutUs.tsx
+++ sections/AboutUs.tsx
@@ -1,11 +1,18 @@
+'use client';
+
 import ScrollReveal from '@/components/ui/AboutScroll'
 import ScrollFloat from '@/components/ui/HeadingScroll'
+import { useLanguage } from '@/context/LanguageContext';
+import { playFair } from '@/lib/fonts';
 import React from 'react'
 
 const AboutUs = () => {
+  const {t, lang} = useLanguage()
+
   return (
     <section className='h-full my-20 max-w-7xl mx-auto flex items-center justify-center flex-col gap-3'>
-        <ScrollFloat
+
+        {lang === "en" ? <ScrollFloat
         animationDuration={1}
         ease='back.inOut(2)'
         scrollStart='center bottom+=50%'
@@ -13,7 +20,7 @@ const AboutUs = () => {
         stagger={0.03}
         >
         About Us
-        </ScrollFloat>
+        </ScrollFloat> : <h3 className={`${playFair.className} text-4xl md:text-5xl my-5`}>معلومات عنا</h3>}
 
         <ScrollReveal
         baseOpacity={1}
@@ -21,12 +28,18 @@ const AboutUs = () => {
         baseRotation={3}
         blurStrength={5}
         >
-        At Ama’s Bakery, every creation is baked with love and passion.
+          {t(` At Ama’s Bakery, every creation is baked with love and passion.
         We craft delightful pastries, cakes, and treats that bring joy to every bite.
         Using only the finest ingredients, we blend tradition with a touch of modern artistry.
         
         Our mission is simple: to make every moment sweeter and every celebration memorable.
-        Step in, indulge, and let your taste buds experience a little piece of happiness.
+        Step in, indulge, and let your taste buds experience a little piece of happiness.`, ` في مخبز آما، كل قطعة تُخبز بحب وشغف.
+نُعدّ معجنات وكعكات وحلويات شهية تُضفي البهجة على كل لقمة.
+باستخدام أجود المكونات فقط، نمزج بين الأصالة ولمسة من الإبداع العصري.
+
+مهمتنا بسيطة: أن نجعل كل لحظة أحلى وكل احتفال لا يُنسى.
+
+تفضلوا بالدخول، واستمتعوا، ودعوا براعم ذوقكم تتذوق لحظات من السعادة.`, lang)}
         </ScrollReveal>
     </section>
   )
