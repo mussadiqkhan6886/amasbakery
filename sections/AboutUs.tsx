5c04f3b7afac8f383b7f59230ca76988605956d2
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-02-13 23:38:33 +0500

    website optimized

--- sections/AboutUs.tsx
+++ sections/AboutUs.tsx
@@ -2,17 +2,20 @@
 
 import CanDoHeader from '@/components/customer/CanDoHeader';
 import { useLanguage } from '@/context/LanguageContext';
+import Image from 'next/image';
 import React from 'react'
 
 const AboutUs = () => {
   const {t, lang} = useLanguage()
 
   return (
-    <section className='h-full my-20 max-w-7xl mx-auto flex items-center justify-center flex-col gap-3'>
-
+    <section className='h-full  max-w-7xl mx-auto flex overflow-hidden items-center justify-center flex-col relative py-20 gap-3'>
+      <div>
+        <Image src={"/580.webp"} alt='background about' width={1000} height={700} className='w-full h-full object-cover object-center absolute opacity-20 inset-0' />
+      </div>
         <CanDoHeader en="About Us" ar="معلومات عنا" />
 
-        <p className="text-center max-w-4xl mx-auto">{t(` At Ama’s Bakery, every creation is baked with love and passion.
+        <p className="text-center max-w-4xl mx-auto px-3 ">{t(` At Ama’s Bakery, every creation is baked with love and passion.
         We craft delightful pastries, cakes, and treats that bring joy to every bite.
         Using only the finest ingredients, we blend tradition with a touch of modern artistry.
         
--- sections/CanDo.tsx
+++ sections/CanDo.tsx
@@ -1,27 +1,27 @@
 import CanDoHeader from '@/components/customer/CanDoHeader'
 import CanDoVideoHeading from '@/components/customer/CanDoVideoHeading'
+import Link from 'next/link'
 
 const CanDo = () => {
   return (
     <section className='my-20 max-w-8xl mx-auto'>
        <CanDoHeader en=' What can we do for you' ar='ما الذي يمكننا تقديمه لكم؟' />
         <section className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-2'>
-            <div className='relative'>
+            <Link href={"/collections/menu"} className='relative'>
                 <div className='absolute inset-0 w-full h-full bg-black/20' />
                 <video className=' md:h-[88vh] w-full object-cover object-center'  src={"/menu.mp4"} muted playsInline autoPlay loop />
                 <CanDoVideoHeading header={`Menu`} ar="قائمة طعام " />
-            </div>
-            <div className='relative'>
+            </Link>
+            <Link href={"/collections/occasion-cakes"} className='relative'>
                 <div className='absolute inset-0 w-full h-full bg-black/20' />
                 <video className='md:h-[88vh] w-full object-cover object-center'  src={"/occasion.mp4"} muted playsInline autoPlay loop />
                 <CanDoVideoHeading header={`Occasion Cakes `} ar='كعك المناسبات ' />
-            </div>
-            <div className='relative'>
+            </Link>
+            <Link href={"/customize-your-cake"} className='relative'>
                 <div className='absolute inset-0 w-full h-full bg-black/20' />
                 <video className='md:h-[88vh] w-full object-cover object-center'  src={"/customize.mp4"} muted playsInline autoPlay loop />
                 <CanDoVideoHeading header={`Customize Your Cake`} ar='تخصيص الكعكة الخاصة بك ' />
-
-            </div>
+            </Link>
         </section>
     </section>
   )
