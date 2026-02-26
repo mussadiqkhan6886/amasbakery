8515e3452098562a42bec96ecd76742fae8fc130
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-02-26 23:24:49 +0500

    changes

--- app/(main)/wedding-cake/page.tsx
+++ app/(main)/wedding-cake/page.tsx
@@ -3,7 +3,7 @@ import { Metadata } from 'next'
 import React from 'react'
 
 export const metadata: Metadata = {
-  title: 'Luxury Wedding Cakes | Custom Bridal Cake Design & Booking',
+  title: 'Luxury Wedding Cakes',
   description: 'Discover our exclusive gallery of handcrafted wedding cakes. From classic elegance to modern designs, book your dream wedding cake consultation with Amas Bakery in alkhobar and dammam.',
   keywords: ['wedding cakes', 'bridal cakes', 'custom wedding cakes', 'wedding cake gallery', 'luxury cakes', 'order wedding cake', "cakes in al-khobar", "cakes in dammam", "wedding cake in saudi", "wedding cake in dammam", "wedding cake in al-khobar", "custom wedding cake in saudi", "custom wedding cake booking"],
   openGraph: {
