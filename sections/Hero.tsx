04bf309238625e898921fc2070a5e917b818d94e
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-03-05 00:05:58 +0500

    done

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -5,14 +5,6 @@ import React from 'react'
 const Hero = () => {
   return (
     <section className="relative w-full h-screen overflow-hidden">
-      {/* <video
-        className="absolute bottom-0 md:top-0 left-0 w-full h-full object-cover"
-        src="/hero.mp4"
-        autoPlay
-        loop
-        muted
-        playsInline
-      /> */}
       <div className='absolute inset-0 h-screen w-full bg-black/10 z-20' />
       <Image alt='hero image amass bakery' priority fetchPriority='high' src="/hero.webp" fill className="object-cover object-center" />
      <HeroHeading />
